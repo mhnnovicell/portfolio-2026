3a166fa4f996926b50b6ba34a3d27a15c65169c3
mhn2012MikkelsPC\Mikkel Hornbech <mikkelhn1@hotmail.com>
2026-01-08 14:57:38 +0100

    New styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from 'next';
+import type { Metadata, Viewport } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import { Analytics } from '@vercel/analytics/next';
@@ -15,6 +15,11 @@ const geistMono = Geist_Mono({
   variable: '--font-geist-mono',
   subsets: ['latin'],
 });
+
+export const viewport: Viewport = {
+  themeColor: 'black',
+  colorScheme: 'dark',
+};
 
 export const metadata: Metadata = {
   title: 'mikkelraev.dk',
